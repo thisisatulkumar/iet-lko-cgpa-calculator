d945a28adfbf60ae9cd7061ed53afff24452c567
thisisatulkumar <theatulkumar11@gmail.com>
2025-12-14 14:25:49 +0530

    fix: grading system

--- src/core/constants/grading.ts
+++ src/core/constants/grading.ts
@@ -1,10 +1,10 @@
 export const MARKS_TO_GRADE = [
-    { min: 90, max: 100, grade: "O", label: "Outstanding", point: 10 },
-    { min: 80, max: 89, grade: "A+", label: "Excellent", point: 9 },
-    { min: 70, max: 79, grade: "A", label: "Very Good", point: 8 },
-    { min: 60, max: 69, grade: "B+", label: "Good", point: 7 },
-    { min: 50, max: 59, grade: "B", label: "Above Average", point: 6 },
-    { min: 40, max: 49, grade: "C", label: "Average", point: 5 },
-    { min: 30, max: 39, grade: "P", label: "Pass", point: 4 },
-    { min: 30, max: 39, grade: "F", label: "Fail", point: 0 },
+    { min: 90, max: 100, grade: "A+", label: "Outstanding", point: 10 },
+    { min: 80, max: 89, grade: "A", label: "Excellent", point: 9 },
+    { min: 70, max: 79, grade: "B+", label: "Very Good", point: 8 },
+    { min: 60, max: 69, grade: "B", label: "Good", point: 7 },
+    { min: 50, max: 59, grade: "C", label: "Above Average", point: 6 },
+    { min: 45, max: 49, grade: "D", label: "Average", point: 5 },
+    { min: 40, max: 44, grade: "E", label: "Pass", point: 4 },
+    { min: 0, max: 39, grade: "F", label: "Fail", point: 0 },
 ] as const;
