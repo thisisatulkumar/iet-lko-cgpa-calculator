c7035d870ad48135c7d852d70b1ed1c35215c2ea
thisisatulkumar <theatulkumar11@gmail.com>
2025-12-22 15:20:52 +0530

    feat: update Home component layout to include GradingTable and SGPAFormula

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import SGPAForm from "@/components/SGPAForm";
+import GradingTable from "@/components/GradingTable";
+import SGPAFormula from "@/components/SGPAFormula";
 
 export default function Home() {
     return (
-        <div className="flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center">
             <SGPAForm />
+
+            <div className="border rounded-md">
+                <GradingTable />
+            </div>
+
+            <div>
+                <SGPAFormula />
+            </div>
         </div>
     );
 }
