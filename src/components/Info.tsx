ad7208113f8d60abb33b0d8f303de7ad88341722
thisisatulkumar <theatulkumar11@gmail.com>
2025-12-22 15:13:46 +0530

    refactor: remove Info component as it is no longer needed

--- src/components/Info.tsx
+++
@@ -1,9 +0,0 @@
-const Info = () => {
-    return (
-        <div className="text-xs text-muted-foreground text-center mt-8">
-            Calculations follow AKTU's grading scheme; results may vary slightly.
-        </div>
-    )
-}
-
-export default Info;
