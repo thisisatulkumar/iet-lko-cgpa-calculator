b2d7e19d38e201288b5718d294b3c3ad16808aba
thisisatulkumar <theatulkumar11@gmail.com>
2025-12-25 22:36:43 +0530

    feat: add tabs for all four years

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,49 @@ import SGPAForm from "@/components/SGPAForm";
 import GradingTable from "@/components/GradingTable";
 import SGPAFormula from "@/components/SGPAFormula";
 
-export default function Home() {
+import {
+    Tabs,
+    TabsContent,
+    TabsList,
+    TabsTrigger,
+} from "@/components/ui/tabs"
+
+const Home = () => {
     return (
         <div className="flex flex-col justify-center items-center">
-            <SGPAForm />
+            <Tabs 
+                defaultValue="1st-year" 
+                className="w-screen md:w-[50vw] flex flex-col items-center pt-4"
+            >
+                <TabsList>
+                    <TabsTrigger value="1st-year">1st Year</TabsTrigger>
+                    <TabsTrigger value="2nd-year">2nd Year</TabsTrigger>
+                    <TabsTrigger value="3rd-year">3rd Year</TabsTrigger>
+                    <TabsTrigger value="4th-year">4th Year</TabsTrigger>
+                </TabsList>
+
+                <TabsContent value="1st-year">
+                    <SGPAForm />
+                </TabsContent>
+
+                <TabsContent value="2nd-year">
+                    <p className="mb-2">
+                        Coming soon...
+                    </p>
+                </TabsContent>
+
+                <TabsContent value="3rd-year">
+                    <p className="mb-2">
+                        Coming soon...
+                    </p>
+                </TabsContent>
+
+                <TabsContent value="4th-year">
+                    <p className="mb-2">
+                        Coming soon...
+                    </p>
+                </TabsContent>
+            </Tabs>
 
             <div className="border rounded-md">
                 <GradingTable />
@@ -17,3 +56,5 @@ export default function Home() {
         </div>
     );
 }
+
+export default Home;
